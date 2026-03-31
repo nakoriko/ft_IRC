36bbb541df5a42a7e2c3ac822d87a1f9aca4ff08
Natalia <natasha.designer9@gmail.com>
2026-03-31 19:32:12 +0200

    PING, QUIT(draft), Server::removeClient() updated

--- srcs/commands/Ping.cpp
+++ srcs/commands/Ping.cpp
@@ -6,7 +6,7 @@
 /*   By: nakoriko <nakoriko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/25 16:34:07 by nakoriko          #+#    #+#             */
-/*   Updated: 2026/03/30 18:00:58 by nakoriko         ###   ########.fr       */
+/*   Updated: 2026/03/31 19:05:44 by nakoriko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,25 @@
 // Check se conessione con client esiste
 void cmd_ping (Server &server, Client &client, const std::vector<std::string> &params, const std::string &trailing) {
 	(void) server;
-	(void) client;
-	(void) params;
-	(void) trailing;
 
-//Server inva ping a client
-//Client puo rispondere con pong
-//se client non risposto  -server puo chiudere consession (eliminando da ovunque)
+
+//Client invia ping a Server e riceve pong ()
+// PING puo essere senza params o con params 
+if(params.empty()) {
+	client.sendMessage("PONG\r\n");
+	return ;
+}
+
+std::string msg = "PONG";
+for(size_t i = 0; i < params.size(); i++) {
+	msg = msg + " " + params[i];
+}
+if(!trailing.empty()) {
+	msg = msg + " :" + trailing;
+}
+client.sendMessage(msg + "\r\n");
+
+//ramo server->client da realizzare dentro  run(), ma non obbligatorio, visto che subject non lo chiede proprio
+//se client non risposto  -server puo chiudere consession (eliminando da ovunque) - 
 
 }
